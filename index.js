9b35ef9ea70bd1d52acd8ee56518cc3eedd1fe61
Ted Sazon <tedsazon@gmail.com>
2018-11-15 21:26:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,21 +4,8 @@ function updateObjectWithKeyAndValue(obj, key, value) {
   return (obj, value)
 
 }
-function updateObjectWithObject(targetObject, updatesObject) {
-  return Object.assign({}, targetObject, updatesObject)
-}
-
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   obj[key] = value
   return obj
-}
-
-
-
-
-
-function deleteFromObjectByKey(obj, key) {
-  delete obj[key]
-  return newobj
 }
