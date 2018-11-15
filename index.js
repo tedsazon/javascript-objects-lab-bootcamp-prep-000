579ff8beeeae52a79ea6ad35f4b59de9b79d85dc
Ted Sazon <tedsazon@gmail.com>
2018-11-15 21:23:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ var recipes = {}
 
 function updateObjectWithKeyAndValue(obj, key, value) {
   return (obj, value)
-}
-
-function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign({}. obj, { [key]: value })
 }
+
 
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
