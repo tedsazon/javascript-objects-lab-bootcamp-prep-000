0709225401d96dae7ba5abdd4d72af039fea7110
Ted Sazon <tedsazon@gmail.com>
2018-11-15 21:13:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ function updateObjectWithKeyAndValue(obj, key, value) {
   return (obj, value)
 }
 
-function updateObjectWithKeyAndValue(obj, key, value) {
-  obj[key] = value
-}
 
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
